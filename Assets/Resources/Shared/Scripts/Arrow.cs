ad7127aaebfc3f756dd01cb11f25088fa832cb09
Michele McColgan <mmccolga@marvls.net>
2023-07-23 19:43:27 -0400

    examples of MARVLS in 2022.3.3f1

---
+++ Assets/Resources/Shared/Scripts/Arrow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Arrow : MonoBehaviour {
+    
+    public Material material;
+
+    void Start() {
+        if (!material) {
+            material =  Resources.Load("Materials/BlueMatte") as Material;
+        }
+    }
+
+    void Update() {
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer meshRenderer in meshRenderers) {
+            meshRenderer.material = material;
+        }
+    }
+}
---
+++ Assets/Resources/Shared/Scripts/ArrowHeadScaler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ArrowHeadScaler : MonoBehaviour
+{
+
+    public Vector3 arrowHeadScale;
+    private Vector3 _parentOriginalScale;
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _transform = transform;
+        
+        if (arrowHeadScale == Vector3.zero)
+            arrowHeadScale = _transform.localScale;
+        
+        _parentOriginalScale = _transform.parent.localScale;
+    }
+
+    private void LateUpdate() {
+        Vector3 currentParentScale = transform.parent.localScale;
+
+
+        // Get the relative difference to the original scale
+        float yDiff = currentParentScale.y / _parentOriginalScale.y;
+
+        // This inverts the scale differences
+        if (yDiff != 0) {
+            yDiff = 1 / yDiff;
+        }
+
+        // Apply the inverted differences to the original scale
+        _transform.localScale = new Vector3(arrowHeadScale.x, arrowHeadScale.y * yDiff, arrowHeadScale.z);
+    }
+}
